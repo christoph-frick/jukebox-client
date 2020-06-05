2b2f5196d4e8f8eb4c848c807a42f3022b34cf45
Christoph Frick <cf@ofnir.net>
2020-06-05 14:57:37 +0200

    Start move to shadow-cljs and antd 4

--- Makefile
+++ Makefile
@@ -1,14 +1,11 @@
 all:
 
-extract:
-	unzip -qqp `lein cp | sed -n 's/.*:\([^:]*antd[^:]*\):.*/\1/p'` cljsjs/antd/production/antd.min.inc.css > resources/public/css/antd.min.inc.css
-
-release: extract
-	lein do clean, cljsbuild once min
+release:
+	./shadow-cljsw release app
 
 deploy: release redeploy
 
 redeploy:
-	rsync -av --delete resources/public/ hwergelmir:/usr/local/www/data/jukebox/
+	scp resources/public/index.html target/main.js node_modules/antd/dist/antd.dark.min.css hwergelmir:/usr/local/www/data/jukebox/
 
 .PHONY: all extract release redeploy deploy
--- src/main/jukebox_client/core.cljs
+++ src/main/jukebox_client/core.cljs
@@ -2,12 +2,21 @@
   (:require [clojure.string :as str]
             [clojure.core.async :as a]
             [rum.core :as rum]
-            [antizer.rum :as ant]
             [citrus.core :as citrus]
             [goog.events :as events]
             [goog.history.EventType :as HistoryEventType]
-            [cljsjs.filesaverjs]
-            [cljsjs.moment])
+            ["filesaverjs" :as FileSaver]
+            ["moment" :as moment]
+            ["antd/es/breadcrumb" :default Breadcrumb]
+            ["antd/es/icon" :default Icon]
+            ["antd/es/button" :default Button]
+            ["antd/es/table" :default Table]
+            ["antd/es/grid" :as Grid]
+            ["antd/es/layout" :default Layout]
+            ["antd/es/row" :default Row]
+            ["antd/es/input" :default Input]
+            ["antd/es/alert" :default Alert]
+            ["@ant-design/icons" :as Icons])
   (:import (goog.history Html5History)))
 
 ;;; config
@@ -54,7 +63,7 @@
 
 (defn download-playlist
     [data]
-    (js/saveAs
+    (FileSaver/saveAs
         (js/Blob.
             [data]
             #js {:type "audio/mpegurl"})
@@ -66,7 +75,6 @@
 
 (defn path-to-root-and-item
   [root path]
-  (print  {:root root :path path})
   (let [path-segments (split-path (js/decodeURI path))
         [root-path-segments item-name] ((juxt butlast last) path-segments)]
     [(str root "/" (join-path root-path-segments))
@@ -84,7 +92,6 @@
     {:type type :url url}))
 
 (defn playlist [root js-item]
-    (print {:root root :js-item js-item})
   (let [items (a/chan (a/dropping-buffer 1000))
         results (a/chan (a/dropping-buffer 25))
         transform (a/pipeline 1 items (mapcat
@@ -134,16 +141,17 @@
 
 (rum/defc Breadcrumbs
   []
-  (ant/breadcrumb
-   (let [parts (history-path-parts)]
+  (rum/adapt-class Breadcrumb
+   (let [parts (history-path-parts)
+         key-gen (fn [part] (str "bc-" part))]
      (for [i (range (count parts))
            :let [path (str (join-path (subvec parts 0 (inc i))) "/")]]
        (if (zero? i)
-         (ant/breadcrumb-item
-          {:id :home}
-          [:a {:href "#/"} (ant/icon {:type :home})])
-         (ant/breadcrumb-item
-          {:id path}
+         (rum/adapt-class (.-Item Breadcrumb)
+          {:key (key-gen :home)}
+          [:a {:href "#/"} (rum/adapt-class (.-HomeOutlined Icons) {})])
+         (rum/adapt-class (.-Item Breadcrumb)
+          {:key (key-gen path)}
           [:a {:href (str "#" path)} (js/decodeURI (nth parts i))]))))))
 
 (rum/defc GoDown
@@ -152,94 +160,95 @@
 
 (rum/defc PlayButton
   [title icon on-click-fn]
-  (ant/button {:title title
-               :icon :caret-right
+  (rum/adapt-class Button {:title title
+               :icon (rum/adapt-class (.-CaretRightOutlined Icons) {})
                :ghost true
                :type :primary
                :on-click on-click-fn}
-              (ant/icon {:type icon})))
+              (rum/adapt-class icon {})))
 
 (defn icon-by-type
   [type-str]
   (case type-str
-    "directory" :folder
-    "file" :file
-    :question))
+    "directory" (.-FolderOutlined Icons)
+    "file" (.-FileOutlined Icons)
+    (.-QuestionCircleOutlined Icons)))
 
 (rum/defc PlayList
   [loading? root path data]
-  (ant/table {:loading loading?
-              :style {:background "white"}
-              :pagination {:position :bottom
-                           :showTotal (fn [total [start end]]
-                                        (str start "-" end " of " total " items"))
-                           :showQuickJumper true}
-              :dataSource data
-              :columns [{:title (PlayButton
-                                 "Play all (recursive)"
-                                 :bars
-                                 (fn []
-                                   (apply playlist (path-to-root-and-item root path))))
-                         :align :center
-                         :width 1
-                         :render (fn [_ item]
-                                   (let [type (aget item "type")]
-                                     (PlayButton
-                                      (str "Play " (case type "directory" "directory (recursive)" "file"))
-                                      (icon-by-type type)
-                                      (fn []
-                                          (playlist (str root "/" path) item)))))}
-                        {:title "Type"
-                         :align :center
-                         :width "8em"
-                         :dataIndex :type
-                         :render (fn [type]
-                                   (ant/icon {:type (icon-by-type type)}))}
-                        {:title "Name"
-                         :dataIndex :name
-                         :sorter (fn [a b]
-                                   (let [f #(aget % "name")]
-                                     (compare (f a) (f b))))
-                         :render (fn [name item]
-                                   (if (= (aget item "type") "directory")
-                                     (GoDown name)
-                                     name))}
-                        {:title "Modified"
-                         :width "12em"
-                         :align "right"
-                         :dataIndex :mtime
-                         :defaultSortOrder "descend"
-                         :sortDirections ["descend" "ascend"]
-                         :sorter (fn [a b]
-                                   (let [f #(-> % (aget "mtime") (js/Date.parse))]
-                                     (compare (f a) (f b))))
+  (rum/adapt-class Table
+                   {:loading loading?
+                    :pagination {:position :bottom
+                                 :showTotal (fn [total [start end]]
+                                                (str start "-" end " of " total " items"))
+                                 :showQuickJumper true}
+                    :dataSource data
+                    :rowKey (fn [item] (str root "/" path "/" (aget item "name")))
+                    :columns [{:title (PlayButton
+                                          "Play all (recursive)"
+                                          (.-BarsOutlined Icons)
+                                          (fn []
+                                              (apply playlist (path-to-root-and-item root path))))
+                               :align :center
+                               :width 1
+                               :render (fn [_ item]
+                                           (let [type (aget item "type")]
+                                               (PlayButton
+                                                   (str "Play " (case type "directory" "directory (recursive)" "file"))
+                                                   (icon-by-type type)
+                                                   (fn []
+                                                       (playlist (str root "/" path) item)))))}
+                              {:title "Type"
+                               :align :center
+                               :width "8em"
+                               :dataIndex :type
+                               :render (fn [type]
+                                           (rum/adapt-class (icon-by-type type) {}))}
+                              {:title "Name"
+                               :dataIndex :name
+                               :sorter (fn [a b]
+                                           (let [f #(aget % "name")]
+                                               (compare (f a) (f b))))
+                               :render (fn [name item]
+                                           (if (= (aget item "type") "directory")
+                                               (GoDown name)
+                                               name))}
+                              {:title "Modified"
+                               :width "12em"
+                               :align "right"
+                               :dataIndex :mtime
+                               :defaultSortOrder "descend"
+                               :sortDirections ["descend" "ascend"]
+                               :sorter (fn [a b]
+                                           (let [f #(-> % (aget "mtime") (js/Date.parse))]
+                                               (compare (f a) (f b))))
 
-                         :render (fn [mtime]
-                                   (-> (js/moment. mtime)
-                                       (.format "YYYY-MM-DD")))}]}))
+                               :render (fn [mtime]
+                                           (-> (moment. mtime)
+                                               (.format "YYYY-MM-DD")))}]}))
 
 (rum/defc App <
   rum/reactive
   [r]
   (let [{:keys [loading? random? error root path filter-term effective-data]} (rum/react (citrus/subscription r [:navigation]))]
-    (ant/layout {:style {:height "100vh"}}
-     (ant/layout-content
+    (rum/adapt-class Layout {:style {:height "100vh"}}
+     (rum/adapt-class (.-Content Layout)
       {:style {:padding "0 2em"}}
       [:div {:style {:margin "2ex 0"}}
-       (ant/row
-        (ant/col {:span 12}
+       (rum/adapt-class Grid/Row {}
+        (rum/adapt-class Grid/Col {:span 12}
                  (Breadcrumbs))
-        (ant/col {:span 12 :align :right}
-                 (ant/input-search {:value filter-term
+        (rum/adapt-class Grid/Col {:span 12 :align :right}
+                 (rum/adapt-class (.-Search Input) {:value filter-term
                                     :placeholder "Filter by name"
                                     :allow-clear true
                                     :style {:width "20em"}
                                     :on-change #(citrus/dispatch! r :navigation :filter (.-value (.-target %)))})
-                 (ant/button {:on-click #(citrus/dispatch! r :navigation :randomize) :clicked true :type (if random? "primary" "")} "Random")
-                 (ant/button {:on-click #(citrus/dispatch! r :navigation :reset) :title "Reset filters"} (ant/icon {:type "rollback"}))))]
+                 (rum/adapt-class Button {:on-click #(citrus/dispatch! r :navigation :randomize) :clicked "true" :type (if random? "primary" "")} "Random")
+                 (rum/adapt-class Button {:on-click #(citrus/dispatch! r :navigation :reset) :title "Reset filters"} (rum/adapt-class (.-RollbackOutlined Icons) {}))))]
       [:div
        (cond
-         error (ant/message-error error)
+         error (rum/adapt-class Alert {:type "error" :message error})
          :else (PlayList loading? root path effective-data))]))))
 
 ;;; controllers
@@ -273,12 +282,12 @@
     {:state (assoc state
                    :random? nil
                    :filter-term term
-                   :effective-data (seq (sort-by #(aget % "name") (filter #(str/includes? (str/lower-case (aget % "name")) lower-case-term) data))))}))
+                   :effective-data (into-array (sort-by #(aget % "name") (filter #(str/includes? (str/lower-case (aget % "name")) lower-case-term) data))))}))
 
 (defmethod navigation :randomize [_ _ {:keys [data] :as state}]
     {:state (assoc state
                    :random? true
-                   :effective-data (seq (take 10 (shuffle data))))})
+                   :effective-data (into-array (take 10 (shuffle data))))})
 
 (defmethod navigation :reset [_ _ {:keys [data] :as state}]
     {:state (assoc state
@@ -326,11 +335,7 @@
        (citrus/dispatch! reconciler :navigation :goto (.-token event))))
     (.setEnabled true)))
 
-(rum/mount (App reconciler)
-           (js/document.getElementById "app"))
-
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(defn main
+  []
+  (rum/mount (App reconciler)
+             (js/document.getElementById "app")))
